e89ecf4777e6e5b41efabfb7d178c4d5f99a831c
Deepu@1234567890 <130851994+kumardeepu110@users.noreply.github.com>
2024-02-11 00:51:18 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const array = [
+    {
+        id:1,
+        name:"deepu"
+    },{
+        id:2,
+        name:"aman"
+    },{
+        id:3,
+        name:"moid"
+    }
+]
+
+// module.exports=array
